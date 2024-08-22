0893608f65707d72a3768db11832af9cbe48ab7d
Mere Frances <meredithflss@gmail.com>
2024-08-23 11:38:35 +1200

    added biography description

--- src/components/sections/Biography.jsx
+++ src/components/sections/Biography.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Biography = () => {
   return (
-    <div>
-      Biography
+    <div className="biography-container">
+      <p>
+      Yayoi Kusama is a visionary Japanese artist known for her immersive, 
+      polka dot-covered installations and bold, surreal works that explore 
+      themes of infinity, mental illness, and the cosmos. Her art spans painting, 
+      sculpture, performance, and installation, often blurring the boundaries 
+      between the real and the imaginary.<br/>Kusama's distinctive style, 
+      characterized by repetitive patterns and vibrant colors, has made 
+      her one of the most influential and celebrated contemporary 
+      artists of our time. Her work is a testament to the power of 
+      art as a form of personal expression and a means of 
+      connecting with the world.
+      </p>
     </div>
   )
 }
