c567899bc39de7f8da338e912af572d0c50d14d6
artcreatezart <abbie@Abbies-MBP.hub>
2024-08-23 09:46:42 +1200

    Linked Biography.jsx, gallery.jsx Home.jsx and Header.jsx

---
+++ src/routes/Links.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../components/pages/Home'
+
+
+
+const Links = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+
+    </Routes>
+  )
+}
+
+export default Links
+
